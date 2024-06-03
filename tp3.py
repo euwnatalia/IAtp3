757fd483964e914507043c78a10c9dd0867499c9
euwnatalia <euwnatalia@gmail.com>
2024-06-03 14:48:25 -0300

    Update tp3.py

--- tp3.py
+++ tp3.py
@@ -1,72 +1,72 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-class HopfieldNetwork:
-    def __init__(self, size):
-        self.size = size
-        self.weights = np.zeros((size, size))
+class RedHopfield:
+    def __init__(self, tamano):
+        self.tamano = tamano
+        self.pesos = np.zeros((tamano, tamano))
     
-    def train(self, patterns):
-        for p in patterns:
-            self.weights += np.outer(p, p)
-        np.fill_diagonal(self.weights, 0)
+    def entrenar(self, patrones):
+        num_patrones = len(patrones)
+        for patron in patrones:
+            patron_bipolar = self._binario_a_bipolar(patron)
+            self.pesos += np.outer(patron_bipolar, patron_bipolar)
+        self.pesos /= num_patrones
+        np.fill_diagonal(self.pesos, 0)
     
-    def predict(self, pattern, steps=5):
-        for _ in range(steps):
-            pattern = np.sign(self.weights @ pattern)
-        return pattern
+    def predecir(self, patron, pasos=10):
+        patron_bipolar = self._binario_a_bipolar(patron)
+        for _ in range(pasos):
+            patron_actualizado = np.sign(self.pesos @ patron_bipolar)
+            patron_actualizado[patron_actualizado == 0] = 1
+            if np.array_equal(patron_actualizado, patron_bipolar):
+                break
+            patron_bipolar = patron_actualizado
+        return self._bipolar_a_binario(patron_bipolar)
     
-def plot_pattern(pattern, title):
-    plt.imshow(pattern.reshape(10, 10), cmap='binary')
-    plt.title(title)
+    def _binario_a_bipolar(self, patron_binario):
+        return np.where(np.array(list(patron_binario), dtype=int) == 0, -1, 1)
+
+    def _bipolar_a_binario(self, patron_bipolar):
+        return ''.join(['1' if x == 1 else '0' for x in patron_bipolar])
+
+def mostrar_patron(patron, titulo, tamano=10):
+    matriz_patron = np.array(list(patron), dtype=int).reshape((tamano, tamano))
+    plt.imshow(matriz_patron, cmap='binary')
+    plt.title(titulo)
+    plt.axis('off')
     plt.show()
 
-# Definición de patrones
-patterns = [
-    np.array([1, 1, 1, 1, 0, 0, 0, 0, 0, 0,
-              1, 0, 0, 1, 0, 0, 0, 0, 0, 0,
-              1, 0, 0, 1, 0, 0, 0, 0, 0, 0,
-              1, 1, 1, 1, 0, 0, 0, 0, 0, 0,
-              0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-              0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-              0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-              0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-              0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-              0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
-    np.array([0, 0, 0, 0, 0, 0, 0, 1, 1, 1,
-              0, 0, 0, 0, 0, 0, 0, 1, 0, 1,
-              0, 0, 0, 0, 0, 0, 0, 1, 0, 1,
-              0, 0, 0, 0, 0, 0, 0, 1, 1, 1,
-              0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-              0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-              0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-              0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-              0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-              0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-]
+def mutar_patron(patron, tasa_mutacion):
+    lista_patron = list(patron)
+    num_mutaciones = int(len(patron) * tasa_mutacion)
+    indices_a_mutar = np.random.choice(len(patron), num_mutaciones, replace=False)
+    for indice in indices_a_mutar:
+        lista_patron[indice] = '1' if lista_patron[indice] == '0' else '0'
+    return ''.join(lista_patron)
 
-# Inicializar y entrenar la red
-hopfield = HopfieldNetwork(size=100)
-hopfield.train(patterns)
+def main():
+    # Definición de patrones (caracteres binarios 10x10)
+    patrones = [
+        '0110100010' * 10,
+        '1001001100' * 10,
+        '1110101110' * 10
+    ]
 
-# Patrones ruidosos para probar
-noisy_pattern = np.array([1, 1, 1, 1, 0, 0, 0, 0, 0, 0,
-                          1, 0, 0, 1, 0, 0, 0, 0, 0, 0,
-                          1, 0, 0, 1, 0, 0, 0, 0, 0, 0,
-                          1, 1, 1, 1, 0, 0, 0, 0, 0, 0,
-                          0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                          0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                          0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                          0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                          0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                          0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    # Inicializar y entrenar la red
+    hopfield = RedHopfield(tamano=100)
+    hopfield.entrenar(patrones)
 
-# Visualización del patrón ruidoso
-plot_pattern(noisy_pattern, "Patrón Ruidoso")
+    for patron in patrones:
+        mutado = mutar_patron(patron, 0.3)
+        recuperado = hopfield.predecir(mutado)
+        
+        print("\nPatrón Original:")
+        mostrar_patron(patron, "Patrón Original")
+        print("\nPatrón Mutado:")
+        mostrar_patron(mutado, "Patrón Mutado")
+        print("\nPatrón Recuperado:")
+        mostrar_patron(recuperado, "Patrón Recuperado")
 
-# Predicción con la red de Hopfield
-predicted_pattern = hopfield.predict(noisy_pattern)
-
-# Visualización del patrón predicho
-plot_pattern(predicted_pattern, "Patrón Predicho")
-
+if __name__ == "__main__":
+    main()
